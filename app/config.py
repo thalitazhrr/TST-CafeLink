76a381a3d20b78fd189f25342decfdc528395c73
thalitazhrr <140157102+thalitazhrr@users.noreply.github.com>
2024-11-25 02:43:25 +0700

    config

--- app/config.py
+++ app/config.py
@@ -1,7 +1,9 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     api_key: str
+    supabase_url: str
+    supabase_key: str
 
     class Config:
         env_file = ".env"
