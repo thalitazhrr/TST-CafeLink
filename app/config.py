b960abe658cea882299ae62a9a4d07bd74ae6822
thalitazhrr <140157102+thalitazhrr@users.noreply.github.com>
2024-11-25 03:20:54 +0700

    fix 2

--- app/config.py
+++ app/config.py
@@ -1,11 +1,13 @@
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
+
+# Load .env file manually
+load_dotenv()
 
 class Settings(BaseSettings):
-    api_key: str
-    supabase_url: str
-    supabase_key: str
-
-    class Config:
-        env_file = ".env"
+    api_key: str = os.getenv("API_KEY")
+    supabase_url: str = os.getenv("SUPABASE_URL")
+    supabase_key: str = os.getenv("SUPABASE_KEY")
 
 settings = Settings()
