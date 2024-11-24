ac5d704fe896745b37a41555f94f9d8aea8c1fcc
thalitazhrr <140157102+thalitazhrr@users.noreply.github.com>
2024-11-25 03:52:02 +0700

    fix deployment

--- app/auth.py
+++ app/auth.py
@@ -1,10 +1,11 @@
 from supabase import create_client
+from app.config import settings
 
-SUPABASE_URL = "https://oeradjopiaxfcymwfjzz.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9lcmFkam9waWF4ZmN5bXdmanp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI0NzQxMjgsImV4cCI6MjA0ODA1MDEyOH0.ik3a7JqIPOEgb1Wuz6yzmHn459Up7I1WAA3bycUlkTY"
-
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase = create_client(settings.supabase_url, settings.supabase_key)
 
 def google_login(id_token: str):
-    response = supabase.auth.sign_in_with_id_token(id_token=id_token, provider="google")
-    return response
+    try:
+        response = supabase.auth.sign_in_with_id_token(id_token=id_token, provider="google")
+        return response
+    except Exception as e:
+        raise Exception(f"Error during Google login: {e}")
